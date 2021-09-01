7e92416a62913d7915f457a4cfe8226e1e42bb11
cococa <cocoooooa@gmail.com>
2021-09-01 17:26:56 +0800

    add render funtion

--- JavaScript/Vue3Test/src/pages/TestRenderFunction.vue
+++ JavaScript/Vue3Test/src/pages/TestRenderFunction.vue
@@ -1,18 +1,27 @@
 <template>  
   <div class="flex flex-1 flex-col">
       <div>TestRenderFunction</div>
-      <RenderComponent>123</RenderComponent>
+      
+      <button @click="changeName()">changeName(通过外部的方法改变子控件的props)</button>
+
+
+      <div>下面开始是子控件RenderComponent</div>  
+      <RenderComponent :name="name" age="123">123</RenderComponent>
+
+
   </div>
 </template>
 
 <script>
 import RenderComponent from '../components/RenderComponent.js'
+import { ref} from  'vue'
+
 export default {
   name: 'xx',
   
   data() {
     return {
-       
+        
     };
   },
   components: {
@@ -24,7 +33,16 @@ export default {
      
   },
   setup(props) {
-    
+      let name = ref("cc");
+
+      function changeName(){
+        name.value  = "cocoa"
+      }
+
+      return {
+        name,
+        changeName
+      }
   }
 };
 </script>
