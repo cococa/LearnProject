8bba180188bccb5534c511bd5da04fac8007eace
cocoa <385811416@qq.com>
2021-10-13 14:38:58 +0800

    add [rust]

---
+++ rust/base/c05/demo.rs
@@ -0,0 +1,24 @@
+struct Reactangle {
+    width : i64,
+    height: i64
+}
+
+impl Reactangle{
+    fn area(&self) -> i64{
+        self.height * self. width
+    }
+    fn can_hold(&self, other : &Reactangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+fn main(){
+        let main  = Reactangle{ width : 100 , height :100};
+        let child = Reactangle {width : 20 , height: 20 };
+
+        let result = main.can_hold(&child);
+        println!("main can hold child ? {}", result);
+
+    }
+
