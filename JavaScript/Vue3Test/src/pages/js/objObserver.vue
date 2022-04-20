b682eebe06543f531ba07056faba2c05a3f7bae3
cococa <cocoooooa@gmail.com>
2022-04-20 20:16:47 +0800

    add some

--- JavaScript/Vue3Test/src/pages/js/objObserver.vue
+++ JavaScript/Vue3Test/src/pages/js/objObserver.vue
@@ -1,5 +1,7 @@
+//
 <template>
   <div class="flex flex-1 flex-col">
+    <div>{{ x }}</div>
     <div v-for="(item, index) in codeArray" :key="index" class="item">
       <vue3-markdown-it :source="item.code" />
       <button @click="testClick(item.methodName)">
@@ -10,8 +12,14 @@
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+import { reactive } from "vue";
 import VueMarkdownIt from "vue3-markdown-it";
 import mixinCode from "../../mixins/mixinCode";
+
+// 参考文档
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Proxy
 
 export default {
   name: "asd",
@@ -54,62 +62,181 @@ export default {
 
         walk(data, value) {
           // Object.keys(data.value).forEach((key) => {
-          const keys = Object.keys(data.value);    
-          for (var i = 0; i < keys.length; i++) {  
-            const key = keys[i]
-            console.log("walk key =>> ", key);
-            value = this.value[key];
-            Object.defineProperty(data, key, {
-              enumerable: true,
-              configurable: true,
-              get() {
-                console.log("get" + key);
-                return value;
-              },
-              set(newValue) {
-                if (newValue === data[key]) {
-                  return;
-                }
-                console.log("set" + key + " " + value + "==>" + newValue);
-                value = newValue;
-              },
-            });
-          };
+          const keys = Object.keys(data.value);
+          for (var i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            this.defineReactive(this, key);
+          }
+        }
+
+        defineReactive(data, key, val) {
+          console.log("walk key =>> ", key);
+          val = this.value[key];
+          Object.defineProperty(data, key, {
+            enumerable: true,
+            configurable: true,
+            get() {
+              console.log("get" + key);
+              return val;
+            },
+            set(newValue) {
+              if (newValue === val) {
+                return;
+              }
+              console.log("set" + key + " " + val + "==>" + newValue);
+              val = newValue;
+            },
+          });
         }
       }
 
       let ob = new Observer({
         name: "shencocoa",
         age: 18,
-        langs:['swift','java']
+        langs: ["swift", "java"],
       });
 
       ob.name = "123";
       console.log(ob.name);
 
       console.log(ob.age);
-      
-      // ob.langs.push('python');
-      // console.log(ob.langs);
-
-
-
+      // 数组的set方法没有触发 , ob.langs = [1,2] , 这样才会触发
+      ob.langs.push("python");
+      // 数组的get 触发
+      console.log(ob.langs);
     },
     base3() {
-      const list = [1, 3, 5, 7];
-      console.log("=>", this);
+      const handler = {
+        get: function (obj, prop) {
+          return prop in obj ? obj[prop] : 37;
+        },
+      };
+      let marker = "定义Proxy 对象";
+      const p = new Proxy({}, handler);
+      p.a = 1;
+      p.b = undefined;
+
+      marker = "";
+      console.log(p.a, p.b); // 1, undefined
+      console.log("c" in p, p.c); // false, 37
+      console.log(p.aaa); // 37
+    },
+    base4() {
+      let target = { a: 1 };
+      let p = new Proxy(target, {});
+      p.a = 37; // 操作转发到目标
+      console.log(target.a); // 37. 操作已经被正确地转发
+    },
+    base5() {
+      //  使用Proxy 实现数据验证
+      const dataValidator = {
+        set(obj, prop, value) {
+          if (prop === "age") {
+            if (!Number.isInteger(value)) {
+              throw new TypeError("The age is not an integer");
+            }
+          }
+          obj[prop] = value;
+          return true;
+        },
+      };
+      const p = new Proxy({}, dataValidator);
+
+      p.age = 12;
+      console.log(p.age); // 12
+      p.age = "十八"; // TypeError: The age is not an integer
+    },
+    base6() {
+      //handler.construct() 方法用于拦截new 操作符. 为了使new操作符在生成的Proxy对象上生效，用于初始化代理的目标对象自身必须具有[[Construct]]内部方法（即 new target 必须是有效的）。
+      const constructorHandler = {
+        construct(target, args) {
+          console.log("constructorHandler log", target, args);
+          return new target(...args);
+        },
+      };
+      const ConstructProxy = new Proxy(function (x, y) {
+        this.x = x;
+        this.y = y;
+      }, constructorHandler);
+
+      new ConstructProxy(); // constructorHandler { x: undefined, y: undefined } [].
+    },
+    base7() {
+      const handler = {
+        /**
+         * target 指向目标对象  target必须是可被调用的。也就是说，它必须是一个函数对象。
+         * thisArg 被调用时的上下文对象。
+         * argumentsList 被调用时的参数数组。
+         */
+        apply(target, thisArg, argumentsList) {
+          console.log(`Calculate sum: ${thisArg}  ${argumentsList}`);
+          return target.apply(thisArg, argumentsList);
+        },
+      };
+
+      function sum(a, b) {
+        return a + b;
+      }
+      const p = new Proxy(sum, handler);
+
+      console.log(sum(1, 2)); // 3
+      console.log(p(1, 2));
+
+      // 上面的是直接对一个方法进行代理，这里想验证下对一个类的方法进行代理，貌似不行啊
+      //  target必须是可被调用的。也就是说，它必须是一个函数对象。
+      // class Person {
+      //   sum(a, b) {
+      //     return a + b;
+      //   }
+      // }
+      // const p1 = new Proxy(new Person(), handler);
+
+      // console.log(p1.sum(1, 2));
+    },
+    base8() {
+      // var propertyNames = Object.getOwnPropertyNames(Person.prototype);
+      // console.log(propertyNames);
+
+      // 模拟对方法数组进行代理
+      function sum(a, b) {
+        for (var i = 0; i < 1e3; i++) {
+          console.log(a + b);
+        }
+        return a + b;
+      }
+
+      const list = [sum];
+
+      const calcMethodUseTimehandler = {
+        apply(target, thisArg, argumentsList) {
+          const currentTime = new Date().getTime();
+          const result = target.apply(thisArg, argumentsList);
+          console.log(
+            `Calculate target use : ${new Date().getTime() - currentTime} ms`
+          );
+          return result;
+        },
+      };
+
       list.forEach((element) => {
-        //箭头函数没有 this。如果访问 this，则会从外部获取。
-        console.log("get name from list foreach @arrow function==>", this.name);
-      });
-      list.forEach(function (item) {
-        //Cannot read properties of undefined (reading 'name')
-        // console.log('get name from list foreach ==>',this.name);
-        console.log(this);
+        const p = new Proxy(element, calcMethodUseTimehandler);
+        console.log(p(1, 2));
       });
     },
   },
-  setup(props) {},
+  setup(props) {
+    const x = ref(0);
+    const y = reactive({
+      a: 1,
+      b: 2,
+    });
+    console.log(x);
+    console.log(y);
+    
+    return {
+      x,
+    };
+  },
 };
 </script>
 
