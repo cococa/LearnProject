95723a177380e606fd207ea47012f784f9078704
cococa <cocoooooa@gmail.com>
2022-11-26 10:30:19 +0800

    123

---
+++ Java/JavaTest/src/main/java/com/cocoa/dangan/BaiduJson.java
@@ -0,0 +1,12 @@
+package com.cocoa.dangan;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
+public class Test {
+
+    @SerializedName("words_result")
+    private Map<String, Words> wordsResult;
+
+}
