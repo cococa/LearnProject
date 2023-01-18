1c9e46436627f70eb5a5cd50a5eb73b1b61bc8a9
cococa <cocoooooa@gmail.com>
2023-01-18 16:56:56 +0800

    123

--- JavaScript/commonTest/src/components/InjectComponent.vue
+++ JavaScript/commonTest/src/components/InjectComponent.vue
@@ -1,30 +1,10 @@
 <template>
-  <div class="flex bg-red-200 justify-center items-center flex-col">
-      <div>红色区域为子控件（InjectComponent）</div>
-      <div>username: {{username}}</div>
-      <div>userInfo: {{userInfo}}</div>
+  <div class="flex inject-bg justify-center items-center flex-col">
+    <InjectComponent></InjectComponent>
   </div>
 </template>
 
-<script>
-import { inject } from 'vue'
-export default {
-  name: 'InjectComponent',
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      count: 0,
-    };
-  },
-  setup(props) {
-      const username  = inject('username','the default username')
-      const userInfo = inject('userInfo')
-      return {
-        username,
-        userInfo
-      }
-  }
-};
+<script setup>
+import { InjectComponent } from "./InjectComponent.vue";
 </script>
+<style scoped></style>
