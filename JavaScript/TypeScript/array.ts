356bfc0dfb8f56b7fb458f670138ecdd1d3eb42b
cococa <cocoooooa@gmail.com>
2023-10-09 20:52:24 +0800

    add some

---
+++ JavaScript/TypeScript/array.ts
@@ -0,0 +1,11 @@
+const array1: number[] = [1];
+console.log(typeof array1);
+array1.push(1);
+console.log(typeof array1);
+
+
+const array2: readonly number[] = [1];
+array2.push(1);
+console.log(array2);
+
+
