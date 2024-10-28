cee9ae7e927411f2eb25d042bd5c05ba3e18f92f
cococa <cocoooooa@gmail.com>
2024-10-28 15:11:29 +0800

    feat: add

---
+++ JavaScript/commonTest/src/pages/js/applyCallBind.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-1 flex-col">
+    <vue3-markdown-it :source="source" />
+  </div>
+</template>
+
+<script lang="ts">
+import VueMarkdownIt from "vue3-markdown-it";
+import { ArrayUtil } from "../../util/index.js";
+
+export default {
+  name: "",
+  components: {
+    VueMarkdownIt,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {},
+  methods: {
+    
+  },
+  setup(props) {},
+};
+</script>
+
+<style scoped>
+.blk {
+  margin-bottom: 20px;
+  background-color: azure;
+  border: 1px solid black;
+}
+</style>
